28b41cca849f87b0e2676cbddb774dff720e7375
lukman bolaji <bolajiakanji21@gmail.com>
2024-04-19 01:58:51 -0700

    implement change count feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sessionCount, setSessionCount] = useState(25);
+  const [breakCount, setBreakCount] = useState(5);
+  const [status, setStatus] = useState("sessio");
+  const [timer, setTimer] = useState("25:00");
+
+  function changeCount(a, b) {
+    if (a === "session") {
+      if (b === "increment") {
+        setSessionCount(sessionCount + 1);
+        status === "session"
+          ? setTimer(() => {
+              return sessionCount + 1 < 10
+                ? "0" + (sessionCount + 1) + ":00"
+                : sessionCount + 1 + ":00";
+            })
+          : "";
+      } else {
+        if (sessionCount > 1) {
+          setSessionCount(sessionCount - 1);
+          status === "session"
+            ? setTimer(() => {
+                return sessionCount - 1 < 10
+                  ? "0" + (sessionCount - 1) + ":00"
+                  : sessionCount - 1 + ":00";
+              })
+            : "";
+        }
+      }
+    } else {
+      if (b === "increment") {
+        setBreakCount(breakCount + 1);
+        status !== "session"
+          ? setTimer(() => {
+              return breakCount + 1 < 10
+                ? "0" + (breakCount + 1) + ":00"
+                : breakCount + 1 + ":00";
+            })
+          : "";
+      } else {
+        if (breakCount > 1) {
+          setBreakCount(breakCount - 1);
+          status !== "session"
+            ? setTimer(() => {
+                return breakCount - 1 < 10
+                  ? "0" + (breakCount - 1) + ":00"
+                  : breakCount - 1 + ":00";
+              })
+            : "";
+        }
+      }
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="container">
+      <div id="break-label">Break Lenght</div>
+      <div id="session-label">Session Length</div>
+      <div
+        id="break-decrement"
+        onClick={() => changeCount("break", "decrement")}
+      >
+        a
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div
+        id="session-decrement"
+        onClick={() => changeCount("session", "decrement")}
+      >
+        b
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div
+        id="break-increment"
+        onClick={() => changeCount("break", "increment")}
+      >
+        c
+      </div>
+      <div
+        id="session-increment"
+        onClick={() => changeCount("session", "increment")}
+      >
+        d
+      </div>
+      <div id="break-length">{breakCount}</div>
+      <div id="session-length">{sessionCount}</div>
+      <div id="timer-label">fdf</div>
+      <div id="time-left">{timer}</div>
+      <div id="start_stop"></div>
+      <div id="reset"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
