706bca441f63c3b0a3ba6fac0a9f25cb5c11a5e5
lukman bolaji <bolajiakanji21@gmail.com>
2024-04-21 10:08:34 -0700

    add countdown feature

--- src/App.jsx
+++ src/App.jsx
@@ -5,56 +5,119 @@ import "./App.css";
 function App() {
   const [sessionCount, setSessionCount] = useState(25);
   const [breakCount, setBreakCount] = useState(5);
-  const [status, setStatus] = useState("sessio");
+  const [status, setStatus] = useState("session");
   const [timer, setTimer] = useState("25:00");
+  const [startStop, setStartStop] = useState(1500);
+  const [timerOn, setTimerOn] = useState(false);
+  
+  const [id, setId] = useState('');
 
   function changeCount(a, b) {
-    if (a === "session") {
-      if (b === "increment") {
-        setSessionCount(sessionCount + 1);
-        status === "session"
-          ? setTimer(() => {
-              return sessionCount + 1 < 10
-                ? "0" + (sessionCount + 1) + ":00"
-                : sessionCount + 1 + ":00";
-            })
-          : "";
-      } else {
-        if (sessionCount > 1) {
-          setSessionCount(sessionCount - 1);
+    if (!timerOn) {
+      if (a === "session") {
+        if (b === "increment") {
+          setSessionCount(sessionCount + 1);
           status === "session"
             ? setTimer(() => {
-                return sessionCount - 1 < 10
-                  ? "0" + (sessionCount - 1) + ":00"
-                  : sessionCount - 1 + ":00";
+                return sessionCount + 1 < 10
+                  ? "0" + (sessionCount + 1) + ":00"
+                  : sessionCount + 1 + ":00";
               })
             : "";
+          status === "session" ? setStartStop((sessionCount + 1) * 60) : null;
+        } else {
+          if (sessionCount > 1) {
+            setSessionCount(sessionCount - 1);
+            status === "session"
+              ? setTimer(() => {
+                  return sessionCount - 1 < 10
+                    ? "0" + (sessionCount - 1) + ":00"
+                    : sessionCount - 1 + ":00";
+                })
+              : "";
+            status === "session" ? setStartStop((sessionCount - 1) * 60) : null;
+          }
         }
-      }
-    } else {
-      if (b === "increment") {
-        setBreakCount(breakCount + 1);
-        status !== "session"
-          ? setTimer(() => {
-              return breakCount + 1 < 10
-                ? "0" + (breakCount + 1) + ":00"
-                : breakCount + 1 + ":00";
-            })
-          : "";
       } else {
-        if (breakCount > 1) {
-          setBreakCount(breakCount - 1);
+        if (b === "increment") {
+          setBreakCount(breakCount + 1);
           status !== "session"
             ? setTimer(() => {
-                return breakCount - 1 < 10
-                  ? "0" + (breakCount - 1) + ":00"
-                  : breakCount - 1 + ":00";
+                return breakCount + 1 < 10
+                  ? "0" + (breakCount + 1) + ":00"
+                  : breakCount + 1 + ":00";
               })
             : "";
+          status !== "session" ? setStartStop((breakCount + 1) * 60) : null;
+        } else {
+          if (breakCount > 1) {
+            setBreakCount(breakCount - 1);
+            status !== "session"
+              ? setTimer(() => {
+                  return breakCount - 1 < 10
+                    ? "0" + (breakCount - 1) + ":00"
+                    : breakCount - 1 + ":00";
+                })
+              : "";
+            status !== "session" ? setStartStop((breakCount - 1) * 60) : null;
+          }
         }
+        j;
       }
-    }
+    } else return null;
   }
+  let stop 
+  stop = startStop 
+      
+  
+  let timein = function () {
+    
+          stop--  
+    
+    if (stop === 0) {
+      setTimer("00.00");
+      if (status === "session") {
+        setStatus("break");
+        stop = breakCount * 60;
+      } else {
+        setStatus("session");
+       stop = sessionCount * 60;
+      }} else {
+      let minutes = Math.floor(stop / 60);
+      minutes = minutes < 10 ? "0" + minutes : minutes; 
+      let seconds = stop % 60;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      setTimer(minutes + ":" + seconds)
+      setStartStop(stop)
+      
+      
+      
+      
+      
+    }
+    
+    
+  }
+  
+  function countDown() {
+   
+    if (!timerOn) {
+      setTimerOn(true);
+      
+  
+     
+   setId((setInterval(timein, 1000)));
+     console.log(id)
+
+ } 
+    else {
+    setTimerOn(false);
+        console.log('here')
+        console.log(id)
+        clearInterval(id);
+
+    }
+    }
 
   return (
     <div id="container">
@@ -88,7 +151,8 @@ function App() {
       <div id="session-length">{sessionCount}</div>
       <div id="timer-label">fdf</div>
       <div id="time-left">{timer}</div>
-      <div id="start_stop"></div>
+      <div id="start_stop" onClick={countDown}>huge
+      </div>
       <div id="reset"></div>
     </div>
   );
